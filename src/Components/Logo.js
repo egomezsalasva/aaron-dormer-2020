59a048d56f2326832d7092048619164b0fed209a
Kike <kike@Kikes-MacBook-Pro.local>
2020-02-10 13:30:31 +0100

    Initial Commit

---
+++ src/Components/Logo.js
@@ -0,0 +1,17 @@
+//Import Libraries
+import React from 'react';
+//Import Images
+import LogoComp from '../aasets/LogoComp.svg'
+
+
+
+
+function Logo() {
+  return (
+    <>
+        
+    </>
+  );
+}
+
+export default Logo;
---
+++ src/Pages/Home.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+
+function Home() {
+  return (
+    <>
+      {/* <SlideshowImage />
+      <SlideshowArrows />
+      <SlideshowPlayButton />
+      <SlideshowTitle />
+      <SlideshowNumber /> */}
+    </>
+  );
+}
+
+export default Home;
