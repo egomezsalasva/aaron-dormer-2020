8fddb1323acbf5808022745774927e65fdecef93
Kike <kike@Kikes-MacBook-Pro.local>
2020-02-10 15:43:08 +0100

    Arrows and Slideshow Image

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,27 +1,56 @@
 //Import Libraries
 import React from 'react'
 import styled from 'styled-components'
+//Import Images
+import keepOnRolling from '../aasets/slideshow/keepOnRolling.png'
 
 
 //Styles
-const SlideshowBox = styled.div`
+const SlideshowImage = styled.img`
   width: 67.0833333vw;
   height: 68.75vh;
-  background: grey;
   position: absolute;
   left: 50%;
   transform: translate(-50%);
   top: calc( 50px + 5.625vh + 5vh );
+  object-fit: cover;
+  object-position: center;
 `
-
+const SlideshowArrowContainer = styled.div`
+  width: 80px;
+  height: 30px;
+  position: absolute;
+  top: calc( 50px + 5.625vh + 5vh + 68.75vh + 5vh - 10px);
+  left: 18.0555556vw;
+`
+const ArrowButton = styled.div`
+  background: var(--aaronGrey75);
+  width: 30px;
+  height: 30px;
+`
+const LeftArrow = styled(ArrowButton)`
+  position: absolute;
+  left: 0; 
+`
+const RightArrow = styled(ArrowButton)`
+  position: absolute;
+  right: 0;
+`
 
 //Main Component
 function Home() {
   return (
     <>
-      <SlideshowBox />
-      {/* <SlideshowImage />
-      <SlideshowArrows />
+      <SlideshowImage src={keepOnRolling} alt=""/>
+      <SlideshowArrowContainer>
+        <LeftArrow>
+
+        </LeftArrow>
+        <RightArrow>
+
+        </RightArrow>
+      </SlideshowArrowContainer>
+      {/*
       <SlideshowPlayButton />
       <SlideshowTitle />
       <SlideshowNumber /> */}
