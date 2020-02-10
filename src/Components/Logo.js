dd89bdcea745b1eb7216eff3b5010bf43a8010b8
Kike <kike@Kikes-MacBook-Pro.local>
2020-02-10 14:02:07 +0100

    Logo Header Styling and Positioning

--- src/Components/Logo.js
+++ src/Components/Logo.js
@@ -1,15 +1,56 @@
 //Import Libraries
-import React from 'react';
+import React from 'react'
+import styled from 'styled-components'
 //Import Images
 import LogoComp from '../aasets/LogoComp.svg'
 
-
-
+//Styled
+const LogoHeaderContainer = styled.div`
+  position: absolute;
+  display: inline-block;
+  left: 50vw;
+  transform: translate(-50%);
+  top: 40px;
+`
+const LogoIcon = styled.img`
+  display: inline-block;
+`
+const TitleBox = styled.div`
+  display: inline-block;
+  height: 25px;
+  background: var(--aaronWhite);
+  padding: 0 20px;
+  margin-left: 15px;
+  position: relative;
+  top: -13px;
+`
+const Title = styled.h3`
+  display: inline-block;
+  line-height: 25px;
+  font-size: 18px;
+  color: var(--aaronBlack);
+  letter-spacing: 1.5px;
+`
+const Role = styled.h3`
+  display: inline-block;
+  font-size: 18px;
+  color: var(--aaronWhite);
+  letter-spacing: 1.5px;
+  margin-left: 15px;
+  position: relative;
+  top: -13px;
+`
 
 function Logo() {
   return (
     <>
-        
+    <LogoHeaderContainer>
+      <LogoIcon src={LogoComp} alt="" />
+      <TitleBox>
+        <Title>Aaron Dormer</Title>
+      </TitleBox>
+      <Role>Film Director</Role>
+    </LogoHeaderContainer>   
     </>
   );
 }
