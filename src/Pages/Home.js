4ad42ed0da7c3af8f4490d304d5451c6360714cf
Kike <kike@Kikes-MacBook-Pro.local>
2020-02-10 15:57:21 +0100

    Arrow Styling

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -3,6 +3,7 @@ import React from 'react'
 import styled from 'styled-components'
 //Import Images
 import keepOnRolling from '../aasets/slideshow/keepOnRolling.png'
+import leftArrow from '../aasets/leftArrow.svg'
 
 
 //Styles
@@ -27,6 +28,17 @@ const ArrowButton = styled.div`
   background: var(--aaronGrey75);
   width: 30px;
   height: 30px;
+  cursor: pointer;
+  transition: all 0.4s ease-in-out; 
+  img{
+    position: absolute;
+    left: 50%;
+    top: 50%;
+    transform: translate(-50%, -50%);
+  }
+  &:hover{
+    background: var(--aaronWhite);
+  }
 `
 const LeftArrow = styled(ArrowButton)`
   position: absolute;
@@ -35,6 +47,12 @@ const LeftArrow = styled(ArrowButton)`
 const RightArrow = styled(ArrowButton)`
   position: absolute;
   right: 0;
+  img{
+   transform: translate(-50%, -50%) rotate(180deg);
+  }
+`
+const SlideshowPlayButton = styled.div`
+  
 `
 
 //Main Component
@@ -42,16 +60,18 @@ function Home() {
   return (
     <>
       <SlideshowImage src={keepOnRolling} alt=""/>
+
       <SlideshowArrowContainer>
         <LeftArrow>
-
+          <img src={leftArrow} alt="left arrow"/>
         </LeftArrow>
         <RightArrow>
-
+         <img src={leftArrow} alt="right arrow"/>
         </RightArrow>
       </SlideshowArrowContainer>
-      {/*
+
       <SlideshowPlayButton />
+      {/*
       <SlideshowTitle />
       <SlideshowNumber /> */}
     </>
